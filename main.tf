b62fa0f8bf0945a3c25c23bf7baadb236f4ce3fc
Kidduu2123 <pavani5696@gmail.com>
2025-07-25 21:00:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "local_file" "filea" {
+filename = "${path.module}/mydir/filea.txt"
+content = var.filename.filea
+}
+
+resource "local_file" "fileb" {
+filename = "$path.module}/mydir/fileb.txt"
+content = var.filename.fileb
+}
