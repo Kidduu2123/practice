97a1599c88f5e939dd917332fe2c95f1a4d9e7d3
Kidduu2123 <pavani5696@gmail.com>
2025-07-25 22:10:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "local_file" "filea" {
-filename = "${path.module}/mydir/filea.txt"
+filename = "${path.module}/mydir1/filea1.txt"
 content = var.filea
 }
 
 resource "local_file" "fileb" {
-filename = "${path.module}/mydir/fileb.txt"
+filename = "${path.module}/mydir2/fileb1.txt"
 content = var.fileb
 }
