30f66cffeb94dd0bf330cfa4f714b883ac0c19ca
Kidduu2123 <pavani5696@gmail.com>
2025-08-20 14:15:57 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "filea" {
-filename = "${path.module}/mydir1/filea1.txt"
-content = var.filea
-}
-
-resource "local_file" "fileb" {
-filename = "${path.module}/mydir2/fileb1.txt"
-content = var.fileb
-}
