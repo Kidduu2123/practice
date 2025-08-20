3cfe044490ad13a09ea4f8b50efe09704b68a3a3
Kidduu2123 <pavani5696@gmail.com>
2025-08-20 20:40:50 +0530

    Update main.tf

--- budget_alert/main.tf
+++ budget_alert/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "main" {
   name                = var.vmss_name
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
-  sku                 = "Standard_DS1_v2"
+  sku                 = "Standard_B1s"
   instances           = 2
   admin_username      = "azureuser"
   admin_password      = "P@ssword1234!"  # Use Key Vault or secure way in production
